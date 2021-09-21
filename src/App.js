6727e018d009e951aac4d73766cb9af396846f3b
Mikkel Frederiksen <5760459@rts-365.dk>
2021-09-21 20:39:34 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import { useState } from 'react';
 import './App.scss';
-import Cases from './Cases/Cases';
-import Contact from './components/Contact/Contact';
-import Cv from './components/CV/CV';
-import Landing from './components/landing/Landing';
-import Scroller from "./components/scroller/scroller";
+
 function App() {
-  var [scrollPosition, setScrollPosition] = useState(0)
   
   return (
     <>
-      <Scroller setState={setScrollPosition}/>
-      <Landing state={scrollPosition}/>
-      <Cv state={scrollPosition}/>
-      <Cases state={scrollPosition}/>
-      <Contact/>
     </>
   );
 }
